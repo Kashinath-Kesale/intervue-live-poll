86af23a6ad91bf2a7cc40ca09d6c631f79951172
Kashinath Kesale <arjunkesale1@gmail.com>
2026-01-07 12:45:58 +0530

    useSocket env variables updated

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -1,18 +1,25 @@
-import {useEffect, useRef} from 'react';
-import {io} from 'socket.io-client';
+import { useEffect, useRef } from "react";
+import { io } from "socket.io-client";
 
-export const useSocket = () =>{
-    const socketRef = useRef(null);
+export const useSocket = () => {
+  const socketRef = useRef(null);
 
-    useEffect(() =>{
-        socketRef.current = io(import.meta.env.VITE_SOCKET_URL);
+  useEffect(() => {
+    socketRef.current = io(
+      import.meta.env.VITE_API_BASE_URL,
+      {
+        transports: ["websocket"],
+      }
+    );
 
-        socketRef.current.emit('join');
+    socketRef.current.on("connect", () => {
+      socketRef.current.emit("join");
+    });
 
-        return() => {
-            socketRef.current.disconnect();
-        };
-    }, []);
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
 
-    return socketRef;
-}
+  return socketRef;
+};
